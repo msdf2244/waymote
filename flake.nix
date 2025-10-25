50fd1f19e18d954acac3b16412697e52332e95fa
Marcus de Freitas <marcus.defreitas@proton.me>
2025-10-25 16:07:34 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    mynvim.url = "github:msdf2244/my-nvim";
+  };
+
+  outputs = { self, nixpkgs, utils, mynvim }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        neovim = mynvim.packages.default;
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy neovim ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      }
+    );
+}
